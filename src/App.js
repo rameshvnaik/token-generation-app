8d5a49f6ddfbac356d13960c3619fb595630473e
ramesh <rameshvnaik2000@gmail.com>
2024-08-23 15:38:14 +0530

    updated push with scss installation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Tokengenerator from './components/Tokengenerator';
 
 function App() {
   return (
     <div className="App">
+      <Tokengenerator/>
+      
     </div>
   );
 }
